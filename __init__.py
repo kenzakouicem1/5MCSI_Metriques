1901ed44b209902baf0ce9f7379ef3af590770e3
kenzakouicem1 <kenza.kouicem@yahoo.com>
2024-09-12 17:04:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,8 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"                                                                                                                                       
+     return render_template("contact")
+  
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
