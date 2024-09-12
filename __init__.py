9da239b487830ef72df45ce07c7cf140a9d0a259
kenzakouicem1 <kenza.kouicem@yahoo.com>
2024-09-12 16:21:01 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ def MaPremiereAPI():
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
- @app.route('/tawarano/')
+@app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
@@ -22,6 +22,6 @@ def meteo():
         dt_value = list_element.get('dt')
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results) 
+    return jsonify(results=results)
 if __name__ == "__main__":
   app.run(debug=True)
